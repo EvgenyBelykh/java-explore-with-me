f241c24b95741810e3618ab708d7ae8ff8e3fc6d
EvgenyBelykh <neonjohn@yandex.ru>
2023-04-06 01:05:51 +0300

    версия 2.0 3 часть ТЗ * Введена система комментариев

--- ewm-service/src/main/java/ru/practicum/apiAdmin/services/CommentServiceAdmin.java
+++ ewm-service/src/main/java/ru/practicum/apiAdmin/services/CommentServiceAdmin.java
@@ -2,8 +2,6 @@ package ru.practicum.apiAdmin.services;
 
 import ru.practicum.common.dto.CommentDto;
 
-import java.util.List;
-
 public interface CommentServiceAdmin {
     CommentDto get(Long comId);
 
--- ewm-service/src/main/java/ru/practicum/common/repositories/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/common/repositories/CommentRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.common.models.Comment;
-import ru.practicum.common.models.Event;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByAuthorId(Long userId, Pageable pageable);
+
     Page<Comment> findAllByEventId(Long userId, Pageable pageable);
 }
